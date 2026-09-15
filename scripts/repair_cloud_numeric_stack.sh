5af31e9eb41ef3631c33e6e655209238f3d3f09e
Yihong Zhang <805482377@qq.com>
2026-09-15 18:16:36 +0800

    Reuse cached PyTorch repair wheels

--- scripts/repair_cloud_numeric_stack.sh
+++ scripts/repair_cloud_numeric_stack.sh
@@ -40,7 +40,7 @@ for stale_package in torch torchvision torchaudio; do
   fi
 done
 
-"$python_bin" -m pip install --no-cache-dir \
+"$python_bin" -m pip install \
   "torch==2.4.1" "torchvision==0.19.1" "torchaudio==2.4.1" \
   --index-url https://download.pytorch.org/whl/cu121
 
